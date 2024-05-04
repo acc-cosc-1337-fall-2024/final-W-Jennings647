fce6f3cdd4db5a05f88ffec23bf2f9c1f308e4e3
William Jennings <56309747+W-Jennings647@users.noreply.github.com>
2024-05-04 21:41:57 +0000

    Question 1: Set up die

--- src/die.h
+++ src/die.h
@@ -1,1 +1,11 @@
 //
+#include<cstdlib>
+using std::rand;
+class die
+{
+    public:
+        int roll(){return (1 + (rand() % (size - 1)));};
+    private:
+        int size{6};
+
+};
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify random")
+{
+	die test;
+	int check;
+	for(auto i = 0; i < 10; i++)
+	{
+		check = test.roll();
+		REQUIRE(1 <= check);
+		REQUIRE(6 >= check);
+	}
 }
